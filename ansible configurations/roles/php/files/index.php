d5a505a1001641178dbc02c61bca7066d88be8cf
Ajay Shenoy <ajayshenoyh@gmail.com>
2019-12-22 12:25:23 -0800

    Adding ansible configurations

---
+++ ansible configurations/roles/php/files/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Welcome to DevOps on AWS</title>
+</head>
+<body>
+    <h1><?php echo "Welcome to devOps on AWS. This text was generated using php 7"?></h1>
+    <h2><?php echo "This change is reflected with anisble pull"?></h2>
+    </body>
+    </html>
